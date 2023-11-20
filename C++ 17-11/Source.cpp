094403aa97cbc7ca89cbec8616d025d667b80ce9
Bogdan <bogd.moskv@gmail.com>
2023-11-20 20:29:59 +0200

    Add new file, edit Source.cpp

--- C++ 17-11/Source.cpp
+++ C++ 17-11/Source.cpp
@@ -7,11 +7,16 @@ void func1() {
 
 }
 
+void func2() {
+	cout << "fun2\n";
+}
+
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
 	cout << "Line1" << endl;
+	cout << "Line 2" << endl;
 	
 
 	return 0;
